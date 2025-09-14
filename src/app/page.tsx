7bdd28778ca23fa822e4630251e941799b1a7ab7
En <graphic.mimage@gmail.com>
2025-09-14 17:04:17 +0700

    Initial commit: Personal Blog Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+         <h1 className="text-5xl font-extrabold text-center sm:text-left">
+          Welcome to <span className="text-purple-600"> My Personal Blog : FSD9-EN </span>
+        </h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        
+      </footer>
+    </div>
+  );
+}
